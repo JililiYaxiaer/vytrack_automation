53e1d7e4ce5165b348eaf1e918a399ef3f68bfae
Alisherb28 <alish.tussupbaev@gmail.com>
2023-03-06 13:42:34 -1000

    Vusal crasher

--- src/test/java/com/vytrack/Runners/CukesRunner.java
+++ src/test/java/com/vytrack/Runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@regression",
+        tags = "@US_1",
         publish = true
 )
 
